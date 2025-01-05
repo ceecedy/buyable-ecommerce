b6152e711f4e81099279da4a3f6f3668d611b793
ceecedy <ceeceddy.pro@gmail.com>
2025-01-05 18:38:38 +0800

    make view for the specific product.

--- src/buyablemain/product/views.py
+++ src/buyablemain/product/views.py
@@ -1,3 +1,10 @@
+# django essentials
 from django.shortcuts import render
+# product model
+from .models import Products
 
-# Create your views here.
+# View specific product 
+def specific_product(request):
+    # specific_product = Products.objects.get()
+    
+    return render(request, 'product/specific_product.html')
