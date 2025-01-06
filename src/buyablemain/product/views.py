5c1835bf69ad4f265d9aeac2072348f34683b01a
ceecedy <ceeceddy.pro@gmail.com>
2025-01-06 10:22:01 +0800

    feat: For Product Lists Made the specific product view dynamic. Made the templates better.

--- src/buyablemain/product/views.py
+++ src/buyablemain/product/views.py
@@ -1,10 +1,17 @@
 # django essentials
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.contrib import messages
 # product model
 from .models import Products
 
 # View specific product 
-def specific_product(request):
-    # specific_product = Products.objects.get()
+def specific_product(request, slug):
+    try:
+        specific_product = get_object_or_404(Products, slugname=slug)
+    except Http404:
+        messages.error(request, 'The item is currently not available.')
+        return render(request, 'product/item_not_available.html')
     
-    return render(request, 'product/specific_product.html')
+    return render(request, 'product/specific_product.html', {'product': specific_product})
+    
